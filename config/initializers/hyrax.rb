0ba01773e7742ab11728f1c02b9ad436657dd3a7
Bess Sadler <bess@curationexperts.com>
2017-10-17 17:28:47 -0400

    Generate Data Set work type

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -3,6 +3,8 @@ Hyrax.config do |config|
   config.register_curation_concern :publication
   # Injected via `rails g hyrax:work ConferenceProceeding`
   config.register_curation_concern :conference_proceeding
+  # Injected via `rails g hyrax:work DataSet`
+  config.register_curation_concern :data_set
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
