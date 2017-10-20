8d1f1a7aa33960024c6e76907fbde18f53cf53c4
Bess Sadler <bess@curationexperts.com>
2017-10-20 13:37:58 -0400

    Deploy to new server as localhost

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,5 +1,5 @@
 # deploys to DCE sandbox
 set :stage, :localhost
 set :rails_env, 'production'
-server '34.227.251.196', user: 'deploy', roles: [:web, :app, :db]
+server '54.80.236.34', user: 'deploy', roles: [:web, :app, :db]
 append :linked_files, "config/environments/production.rb"
