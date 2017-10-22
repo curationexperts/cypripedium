80aab36d0be9367da6bbde57cddf397db3e4ea84
Bess Sadler <bess@curationexperts.com>
2017-10-22 13:55:54 -0400

    localhost should deploy to localhost

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,5 +1,5 @@
 # deploys to DCE sandbox
 set :stage, :localhost
 set :rails_env, 'production'
-server '54.80.236.34', user: 'deploy', roles: [:web, :app, :db]
+server '127.0.0.1', user: 'deploy', roles: [:web, :app, :db]
 append :linked_files, "config/environments/production.rb"
