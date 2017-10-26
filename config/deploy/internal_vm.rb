5e37a240fcaf6f70a76340f7c7ae37ec649dca29
Bess Sadler <bess@curationexperts.com>
2017-10-25 21:27:33 -0400

    Use localhost as the ansible bootstrapping stage name

--- config/deploy/internal_vm.rb
+++
@@ -1,4 +0,0 @@
-# deploys to DCE sandbox
-set :stage, :internal_vm
-set :rails_env, 'production'
-server '127.0.0.1', user: 'deploy', roles: [:web, :app, :db]
---
+++ config/deploy/localhost.rb
@@ -0,0 +1,5 @@
+# capistrano bootstrapping deploy, run by ansible
+# this *must* be named localhost
+set :stage, :localhost
+set :rails_env, 'production'
+server '127.0.0.1', user: 'deploy', roles: [:web, :app, :db]
