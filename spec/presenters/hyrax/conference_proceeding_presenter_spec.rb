4dd266890733693d42b2ef378a45c7b27f4a05ac
val99erie <val99erie@gmail.com>
2018-01-16 10:22:10 -0600

    Code cleanup: Removed empty pending specs

--- spec/presenters/hyrax/conference_proceeding_presenter_spec.rb
+++
@@ -1,9 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work ConferenceProceeding`
-require 'rails_helper'
-
-RSpec.describe Hyrax::ConferenceProceedingPresenter do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
