4b7cc5c2e2dd5f4139398070057e8b59bdecab16
janneller <research@cssadmins-air.red.minneapolisfed.org>
2018-01-19 09:35:35 -0600

    updating capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.1"
+lock "3.10.1"
 
 set :application, "cypripedium"
 set :repo_url, "https://github.com/curationexperts/cypripedium.git"
