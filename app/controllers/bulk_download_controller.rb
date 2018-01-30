5868e779cef89c90797ca8013efaed2dc7936b70
Jamie Little <jamie@jamielittle.org>
2018-01-30 09:09:10 -0500

    Use ApplicationJob to perform the BulkDownload.

--- app/controllers/bulk_download_controller.rb
+++ app/controllers/bulk_download_controller.rb
@@ -1,6 +1,15 @@
+# coding: utf-8
 class BulkDownloadController < ApplicationController
   def download
-    bulk_download = BulkDownload.new(params[:id])
-    send_data(bulk_download.read_zip, filename: "#{params[:id]}.zip", type: 'application/zip')
+    send_data(BulkDownloadJob.perform_now(params[:id]), filename: download_file_name, type: 'application/zip')
   end
+
+  private
+
+    def download_file_name
+      doc = SolrDocument.find(params[:id])
+      title = Shellwords.escape(doc.title[0][0..30].gsub(/\s+/, ""))
+      date = Time.now.in_time_zone.strftime('%Y-%m-%d')
+      "#{title}_#{date}.zip"
+    end
 end
