6871b8382e18ccad950654c6aa5c6ab5a51cfb1c
val99erie <val99erie@gmail.com>
2018-02-06 13:03:59 -0600

    Mark link as download to avoid weird browser behavior after download

--- app/helpers/work_zips_helper.rb
+++ app/helpers/work_zips_helper.rb
@@ -1,7 +1,7 @@
 module WorkZipsHelper
   def display_work_zip_controls(work_zip)
     if work_zip.file_path && File.exist?(work_zip.file_path)
-      link_to 'Download the zip file', main_app.download_zip_path(work_zip.work_id)
+      link_to 'Download the zip file', main_app.download_zip_path(work_zip.work_id), download: true
     elsif work_zip.status == :unavailable
       button_to 'Create the Zip', main_app.create_zip_path(work_zip.work_id)
     elsif work_zip.status != :completed
