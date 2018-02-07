1ba0b2023243a58c7cfa4cebaa239020a19927bb
Jamie Little <jamie@jamielittle.org>
2018-02-07 09:32:22 -0600

    Add automation using `whenever` to delete temp files
    This commit adds the whenever gem and follows the
    instructions at
    https://curationexperts.github.io/playbook/every_project/cleanup_temp_files.htmlto clean up temp files.
    
    Closes #126

---
+++ config/schedule.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+# Delete blacklight saved searches
+every :day, at: '11:55pm' do
+  rake 'blacklight:delete_old_searches[1]'
+end
+
+# Remove files in /tmp owned by the deploy user that are older than 7 days
+every :day, at: '1:00am' do
+  command '/usr/bin/find /tmp -type f -mtime +7 -user deploy -execdir /bin/rm – {} \\;'
+end
+
+# Remove files in default directory for storing zipped download files owned by the deploy user that are older than 7 days
+every :day, at: '1:00am' do
+  command "/usr/bin/find #{Rails.root.join('tmp', 'zip_exports')} -type f -mtime +7 -user deploy -execdir /bin/rm – {} \\;" # rubocop:disable Rails/FilePath
+end
