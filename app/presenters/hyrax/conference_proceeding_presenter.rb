e4d7a425f67eab14435527a6904da353d22d5e04
Jamie Little <jamie@jamielittle.org>
2018-02-07 10:15:12 -0600

    Add attributes to presenters
    The attributes were only included in the `Publication` presenter.
    This commit adds them to the two other work types.

--- app/presenters/hyrax/conference_proceeding_presenter.rb
+++ app/presenters/hyrax/conference_proceeding_presenter.rb
@@ -3,5 +3,7 @@
 module Hyrax
   class ConferenceProceedingPresenter < Hyrax::WorkShowPresenter
     include ::Hyrax::HasZip
+
+    Attributes.to_a.each { |term| delegate term, to: :solr_document }
   end
 end
