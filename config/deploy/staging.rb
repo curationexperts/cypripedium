d10466c35457f5421ce9f896ed206478aeb20880
janneller <jannelle.ruswick@mpls.frb.org>
2018-02-27 09:58:38 -0600

    Changing IP for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 # deploys to FRBM AWS staging
 set :stage, :staging
 set :rails_env, 'production'
-server '54.147.55.67', user: 'deploy', roles: [:web, :app, :db]
+server '34.207.158.90', user: 'deploy', roles: [:web, :app, :db]
