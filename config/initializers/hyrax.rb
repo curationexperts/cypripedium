f3af065a745396b3e364ae59b9990a6160a200f2
janneller <jannelle.ruswick@mpls.frb.org>
2018-03-05 11:38:56 -0600

    updating table sizes, footer links, banner image config.

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -122,7 +122,7 @@ Hyrax.config do |config|
   # config.audit_user_key = 'audituser@example.com'
   #
   # The banner image. Should be 5000px wide by 1000px tall
-  # config.banner_image = 'https://cloud.githubusercontent.com/assets/92044/18370978/88ecac20-75f6-11e6-8399-6536640ef695.jpg'
+  config.banner_image = 'https://raw.githubusercontent.com/MPLSFedResearch/cypripedium/master/app/assets/images/chalkboardcrop.png'
 
   # Temporary paths to hold uploads before they are ingested into FCrepo
   # These must be lambdas that return a Pathname. Can be configured separately
