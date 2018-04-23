11ad643d575fe8cef8aa9671f40704e374fc7402
janneller <jannelle.ruswick@mpls.frb.org>
2018-04-23 11:35:52 -0500

    Adding sort by title and trying to fix searching.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -30,7 +30,7 @@ class CatalogController < ApplicationController
     config.default_solr_params = {
       qt: "search",
       rows: 10,
-      qf: "title_tesim description_tesim creator_tesim keyword_tesim is_part_of_tesim issue_number_tesim abstract_tesim"
+      qf: "title_tesim description_tesim creator_tesim keyword_tesim abstract_tesim"
     }
 
     # solr field configuration for document/show views
@@ -41,9 +41,9 @@ class CatalogController < ApplicationController
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     config.add_facet_field solr_name("creator", :facetable), collapse: false, limit: 5
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name("resource_type", :facetable), limit: 5
     config.add_facet_field solr_name("subject", :facetable), limit: 5
+    config.add_facet_field solr_name("file_format", :facetable), limit: 5
     # End Facet Fields
 
     # The generic_type isn't displayed on the facet list
@@ -57,15 +57,15 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
+    config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false
     config.add_index_field solr_name("creator", :stored_searchable), itemprop: 'creator', link_to_search: solr_name("creator", :facetable)
     config.add_index_field solr_name("series", :stored_searchable), label: "Series"
     config.add_index_field solr_name("issue_number", :stored_searchable), label: "Number"
     config.add_index_field solr_name("date_created", :stored_sortable, type: :date), itemprop: 'dateCreated', helper_method: :human_readable_date
-    config.add_index_field solr_name("keyword", :stored_searchable)
     config.add_index_field solr_name("abstract", :stored_searchable)
-    config.add_index_field solr_name("description", :stored_searchable)
+    config.add_index_field solr_name("description", :stored_searchable), itemprop: 'description', helper_method: :iconify_auto_link
+    config.add_index_field solr_name("keyword", :stored_searchable), itemprop: 'keywords', link_to_search: solr_name("keyword", :facetable)
     config.add_index_field solr_name("subject", :stored_searchable), itemprop: 'about', link_to_search: solr_name("subject", :facetable)
-    config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -227,7 +227,7 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('series') do |field|
-      solr_name = solr_name("series", :stored_searchable)
+      solr_name = solr_name("is_part_of", :stored_searchable)
       field.solr_local_parameters = {
         qf: solr_name,
         pf: solr_name
@@ -238,7 +238,8 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     # label is key, solr field is value
-    config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance"
+    config.add_sort_field "score desc, title_si asc", label: "relevance"
+    config.add_sort_field 'title_si asc', label: 'title'
     config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u25BC"
     config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u25B2"
     config.add_sort_field "#{modified_field} desc", label: "date modified \u25BC"
