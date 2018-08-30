9588573ef08016f74c62d985562bf174d0f70e0c
Ruswick <irjmrm03@irjmr03al0.red.minneapolisfed.org>
2018-08-30 16:04:38 -0500

    Fixing syntax errors

--- app/forms/hyrax/dataset_form.rb
+++ app/forms/hyrax/dataset_form.rb
@@ -12,6 +12,7 @@ module Hyrax
       result[:related_url].map! { |related_url| RDF::Markdown::Literal.new(related_url) }
       result
     end
+    
     def self.model_attributes(form_params)
       result = super
       result[:description].map! { |description| RDF::Markdown::Literal.new(description) }
