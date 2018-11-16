f3bb8b933385866891c8db1b186e9cafdec0399b
Jamie Little <jamie@jamielittle.org>
2018-11-15 18:24:57 -0600

    Change bag model to create multi-work bags
    This changes the `Bag` model so that it creates
    a single bag with a list of work ids. The bag
    contains the contents of the works in folders
    that have their work id.
    
    Connected to #191

--- spec/models/bag_spec.rb
+++ spec/models/bag_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 require 'fileutils'
 
 RSpec.describe Bag, type: :model do
-  subject(:work_bag) { described_class.new(work_id: publication.id, time_stamp: time_stamp) }
+  subject(:work_bag) { described_class.new(work_ids: [publication.id, publication2.id], time_stamp: time_stamp) }
 
-  let(:tar_size) { 104_448 }
+  let(:tar_size) { 202_752 }
   let(:time_stamp) { 109_092 }
-  let(:file_path) { Rails.root.join('tmp', 'bags', publication.id) }
+  let(:file_path) { Rails.root.join('tmp', 'bags') }
 
   let(:pdf_file) do
     File.open(file_fixture('pdf-sample.pdf')) { |file| create(:file_set, content: file) }
@@ -20,6 +20,10 @@ RSpec.describe Bag, type: :model do
     create(:publication, title: ['My Publication'], file_sets: [pdf_file, image_file])
   end
 
+  let(:publication2) do
+    create(:publication, title: ['My Publication 2'], file_sets: [pdf_file, image_file])
+  end
+
   describe '#create' do
     before do
       FileUtils.rm_rf(file_path)
@@ -28,16 +32,11 @@ RSpec.describe Bag, type: :model do
 
     after do
       FileUtils.rm_rf(file_path)
-      FileUtils.rm_rf("#{file_path}.tar.gz")
     end
 
     context 'a work file attached files' do
-      it 'creates a bag with the files' do
+      it 'creates a bag from the works' do
         work_bag.create
-
-        # Expect the tar file to be created
-        # and expect it to be the correct size
-        expect(File.exist?("#{work_bag.bag_path}.tar")).to eq true
         expect(File.size("#{work_bag.bag_path}.tar")).to eq tar_size
       end
     end
