d7b3a6aa0d42a7aa74d6466dc48e3987f3bf44da
Jamie Little <jamie@jamielittle.org>
2018-11-20 12:14:58 -0600

    Add a notification after bag creation
    This commit changes the bag job so that it will
    send a notification containing a link to the bag
    to the user who created the job, when the bag is
    done being created.
    
    Connected to #192

--- app/jobs/bag_job.rb
+++ app/jobs/bag_job.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 class BagJob < ActiveJobStatus::TrackableJob
-  def perform(work_ids:, time_stamp: Time.now.to_i)
-    bag = Bag.new(work_ids: work_ids, time_stamp: time_stamp)
-    bag.create
+  after_perform :after_bag_creation
+
+  attr_reader :bag, :user
+
+  def perform(work_ids:, time_stamp: Time.now.to_i, user:)
+    @user = user
+    @bag = Bag.new(work_ids: work_ids, time_stamp: time_stamp)
+    @bag.create
   end
+
+  private
+
+    def after_bag_creation
+      bag_file_name = @bag.bag_path.split.last.to_s
+      @user.send_message(@user,
+                         "Your bag has been created and can be downloaded <a href='/bag/download/#{bag_file_name}'>here</a>.",
+                         "Your BagIt archive is ready")
+    end
 end
