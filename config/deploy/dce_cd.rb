f55d178168e2135c7d0e3b02010053c192fed73c
Bess Sadler <bess@curationexperts.com>
2018-11-26 10:52:35 -0500

    Continuous deploy with Travis-CI
    Following this guide:
    https://curationexperts.github.io/playbook/production/continuous_deployment.html
    
    Use an encrypted ssh key to auto-deploy to dce-cd after every
    green merge to the master branch.

--- config/deploy/dce_cd.rb
+++ config/deploy/dce_cd.rb
@@ -1,5 +1,17 @@
-# deploys to dce qa
+# deploys to dce cd
 set :stage, :dce_cd
 set :rails_env, 'production'
 server 'cypripedium-cd.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
 append :linked_files, ".env.production"
+set :ssh_options, keys: ["cypripedium_cd_deploy"] if File.exist?("cypripedium_cd_deploy")
+
+# Capistrano passenger restart isn't working consistently,
+# so restart apache2 after a successful deploy, to ensure
+# changes are picked up.
+namespace :deploy do
+  after :finishing, :restart_apache do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, :apache2
+    end
+  end
+end
