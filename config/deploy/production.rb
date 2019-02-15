d0e4da570f28ccb48de2e9e237577eb70a88f090
Jamie Little <jamie@jamielittle.org>
2019-02-15 13:14:46 -0600

    Change prod deploy IP
    This changes the cap deploy for prod
    to use the Elastic IP.
    
    Connected to #290

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # deploys to FRBM AWS production
 set :stage, :production
 set :rails_env, 'production'
-server '54.235.59.76', user: 'deploy', roles: [:web, :app, :db]
+server '3.208.81.15', user: 'deploy', roles: [:web, :app, :db]
