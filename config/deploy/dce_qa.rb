5e540a908a174e56c7aaed841c59294059bf2db8
Jamie Little <jamie@jamielittle.org>
2019-02-19 18:11:14 +0000

    Update QA deploy target (#312)

--- config/deploy/dce_qa.rb
+++ config/deploy/dce_qa.rb
@@ -3,5 +3,5 @@
 # deploys to dce qa
 set :stage, :dce_qa
 set :rails_env, 'production'
-server 'cypripedium-qa.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
+server 'qa-cypripedium.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
 append :linked_files, ".env.production"
