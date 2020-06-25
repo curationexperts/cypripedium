8d5ac3114bd56bbb01cca32a617baf29b559965d
Bess Sadler <bess@curationexperts.com>
2020-06-25 13:48:14 -0400

    Sort by issue number only if no user query
    If a user is performing a search, we want
    to use the default sort order: relevancy.
    However, if there is no query present,
    then relevancy doesn't mean anything.
    In that case, sort by issue number so that
    collections will show most recent issues
    at the top.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,6 +5,18 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Hydra::AccessControlsEnforcement
   include Hyrax::SearchFilters
 
+  self.default_processor_chain += [:sort_by_issue_number_when_no_query]
+
+  ##
+  # Within a collection show page, we want items to be ordered by issue number.
+  # So, if there is no query (solr_parameters["q"]), set the sort to issue number descending.
+  # However, if there IS a query then don't change the sort order, let it stay
+  # sorting by relevance.
+  def sort_by_issue_number_when_no_query(solr_parameters)
+    issue_number_sort = "issue_number_ssi desc, system_modified_dtsi desc"
+    solr_parameters["sort"] = issue_number_sort if solr_parameters["q"].nil?
+  end
+
   ##
   # @example Adding a new step to the processor chain
   #   self.default_processor_chain += [:add_custom_data_to_query]
