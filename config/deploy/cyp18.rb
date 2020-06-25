d1de9fc1725e3981ab1c3c9d89ded7001f776bd6
Bess Sadler <bess@curationexperts.com>
2020-06-25 09:33:42 -0400

    Remove outdated deploy targets
    Update capistrano version number in deploy.rb

--- config/deploy/cyp18.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-set :stage, :cyp18
-set :rails_env, 'production'
-server 'cyp18.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
-append :linked_files, ".env.production"
--- config/deploy/dce_qa.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# deploys to dce qa
-set :stage, :dce_qa
-set :rails_env, 'production'
-server 'qa-cypripedium.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
-append :linked_files, ".env.production"
