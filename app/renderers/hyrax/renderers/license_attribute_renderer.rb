d6ef63be6ade65c3636249b3dca262a071a2aef7
Rachel M. Kadel <rachelkg@gmail.com>
2020-06-26 10:47:31 -0400

    Style check

--- app/renderers/hyrax/renderers/license_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/license_attribute_renderer.rb
@@ -7,7 +7,7 @@ module Hyrax
       private
 
       ##
-      # Overridden from hyrax to re-enable standard microdata support. 
+      # Overridden from hyrax to re-enable standard microdata support.
       # Special treatment for license/rights.  A URL from the Hyrax gem's config/hyrax.rb is stored in the descMetadata of the
       # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
       def attribute_value_to_html(value)
