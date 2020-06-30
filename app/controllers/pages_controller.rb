4bd5879a89ec82737c0b6f72616388cb5171b8cc
Bess Sadler <bess@curationexperts.com>
2020-06-30 16:16:55 -0400

    Render a static page for 404 errors
    This should eliminate the error we're getting in honeybadger: Missing template pages/error_404

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@
 class PagesController < ApplicationController
   def error_404
     flash[:alert] = 'The page you are looking for does not exist.'
-    render status: 404
+    render file: Rails.root.join('public', '404.html'), status: 404
   end
 end
