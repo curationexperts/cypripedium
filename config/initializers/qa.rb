69c291cc7792f8380cdb5346e4003a41a9d75057
Max Kadel <max@curationexperts.com>
2021-04-01 10:26:29 -0400

    Upgrade QA gem to allow for LOC requests

--- config/initializers/qa.rb
+++ config/initializers/qa.rb
@@ -3,7 +3,7 @@ Qa.config do |config|
   # When enabled, CORS headers will be added to the responses for search and show.  `OPTIONS` method will also be supported.
   # Uncomment one of the lines below to enable or disable CORS headers.  This configuration defaults to disabled when not set.
   # More information on CORS headers at: https://fetch.spec.whatwg.org/#cors-protocol
-  # config.enable_cors_headers
+  config.enable_cors_headers
   # config.disable_cors_headers
 
   # Provide a token that allows reloading of linked data authorities through the controller
