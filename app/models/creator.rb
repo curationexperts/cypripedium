3db4606da1261a7964536c74c796273c078a7123
fnibbit <rachel.kadel@curationexperts.com>
2021-04-02 11:24:36 -0400

    Add validations for creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 class Creator < ApplicationRecord
+  validates :repec, :viaf, uniqueness: {message: "id already in the system", allow_blank: :true}
+  validates :display_name, presence: :true
 end
--- spec/models/creator_spec.rb
+++ spec/models/creator_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Creator, type: :model do
     expect(creator.id).not_to be nil
     expect(creator.display_name).to eq "Allen, Stephen G."
   end
+  it "doesn't allow an empty display_name" do
+    expect do
+      described_class.create
+    end.to raise_error(ActiveRecord::NotNullViolation, /ERROR:  null value in column \"display_name\"/)
+  end
+  it "doesn't allow a blank display_name" do
+    expect do
+      described_class.create!(display_name: "")
+    end.to raise_error(ActiveRecord::RecordInvalid, /Validation failed: Display name can't be blank/)
+  end
+  it "doesn't allow duplicate RePEc ids" do
+    creator = described_class.create(display_name: "Allen, Stephen G.", repec: "pal73")
+    expect do
+      described_class.create!(display_name: "Allen, Stephen Gomes", repec: "pal73")
+    end.to raise_error(ActiveRecord::RecordInvalid, /Validation failed: Repec id already in the system/)
+  end
+  it "doesn't allow duplicate VIAF ids" do
+    creator = described_class.create(display_name: "Allen, Stephen G.", viaf: "789875")
+    expect do
+      described_class.create!(display_name: "Allen, Stephen Gomes", viaf: "789875")
+    end.to raise_error(ActiveRecord::RecordInvalid, /Validation failed: Viaf id already in the system/)
+  end
 end
