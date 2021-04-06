6fe55696a94de3c21df80d59be5211ef6dc114ee
Max Kadel <max@curationexperts.com>
2021-04-06 14:07:00 -0400

    Return search results based on QA autocomplete expectations
    - Rubocop
    - QA assumes you will have a hash with a label, which it uses to 
    populate autocomplete lists

--- app/authorities/qa/authorities/local_names.rb
+++ app/authorities/qa/authorities/local_names.rb
@@ -1,15 +1,26 @@
+# frozen_string_literal: true
 module Qa::Authorities
   class LocalNames < Qa::Authorities::Base
     attr_reader :subauthority
     # sanitize?
-    def search(_q)
-      Creator.where('lower(display_name) like ?', "#{_q.downcase}%").limit(25)
+    def search(q)
+      results = Creator.where('lower(display_name) like ?', "#{q.downcase}%").limit(25)
+      results.map do |result|
+        {
+          id: result.id,
+          label: result.display_name
+        }
+      end
     end
+
     # sanitize?
     def find(id)
-      record = Creator.find_by(id: id)
+      record = Creator.find(id)
       return nil unless record
-      record
+      {
+        id: record.id,
+        label: record.display_name
+      }
     end
   end
 end
--- app/authorities/qa/authorities/local_names/generic_authority.rb
+++ app/authorities/qa/authorities/local_names/generic_authority.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Qa::Authorities
   class LocalNames::GenericAuthority < Base
   end
