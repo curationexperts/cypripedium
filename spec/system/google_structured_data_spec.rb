6a8e0909a2e49b105a1a54f80f426d96b7e67503
fnibbit <rachel.kadel@curationexperts.com>
2021-04-09 16:31:31 -0400

    Combine related tests to speed up suite

--- spec/system/google_structured_data_spec.rb
+++ spec/system/google_structured_data_spec.rb
@@ -5,30 +5,17 @@ include Warden::Test::Helpers
 RSpec.describe 'Use structured data that Google can parse', type: :system, clean: true, js: true do
   context "datasets" do
     let(:work) { FactoryBot.create(:populated_dataset) }
-    it "marks creators with schema.org tags" do
+    it "marks up with schema.org tags", :aggregate_failures do
       visit "/concern/datasets/#{work.id}"
       creators = page.all(:css, "[itemprop='creator']")
       expect(creators.first.text).to eq "McGrattan, Ellen R."
       expect(creators.last.text).to eq "Prescott, Edward C."
-    end
-
-    it "marks abstract with schema.org tags" do
-      visit "/concern/datasets/#{work.id}"
       expect(page.find(:css, '[itemprop="abstract"]').text).to eq "This is my abstract"
       expect(page).not_to have_selector('[itemprop="abstract"][itemtype]')
-    end
-    it "marks identifier with schema.org tags" do
-      visit "/concern/datasets/#{work.id}"
       expect(page.find(:css, '[itemprop="identifier"]').text.strip).to eq "https://doi.org/10.21034/sr.600"
       expect(page).not_to have_selector('[itemprop="identifier"][itemtype]')
-    end
-    it "marks description with schema.org tags" do
-      visit "/concern/datasets/#{work.id}"
       expect(page.find(:css, '[itemprop="description"]').text.strip).to eq "This is my description"
       expect(page).not_to have_selector('[itemprop="description"][itemtype]')
-    end
-    it "marks license with schema.org tags" do
-      visit "/concern/datasets/#{work.id}"
       expect(page.find(:css, '[itemprop="license"][itemprop][itemtype="http://schema.org/CreativeWork"]').text.strip).to eq "Creative Commons BY-NC Attribution-NonCommercial 4.0 International"
     end
   end
