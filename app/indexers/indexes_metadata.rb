cd12bdeb56a8d27269d46ed72df48703c6d110de
Max Kadel <max@curationexperts.com>
2021-04-13 10:57:17 -0400

    Ensure we do not overwrite existing data for creators
    Co-Authored-By: Rachel Kadel <rachel.kadel@curationexperts.com>

--- app/indexers/indexes_metadata.rb
+++ app/indexers/indexes_metadata.rb
@@ -5,11 +5,18 @@ module IndexesMetadata
     super.tap do |solr_doc|
       solr_doc['title_ssi'] = object.title.first
       solr_doc['date_created_ssi'] = object.date_created.first
-      creator_names = object.creator_id.map do |id|
-        Creator.find(id).display_name
-      end
-      solr_doc['creator_tesim'] = creator_names
-      solr_doc['alpha_creator_tesim'] = creator_names.sort
+      solr_doc['creator_tesim'] = creator_names(object)
+      solr_doc['alpha_creator_tesim'] = creator_names(object).sort
     end
+  end
+
+  def creator_names(object)
+    @creator_names ||= if object.creator_id.present?
+                         object.creator_id.map do |id|
+                           Creator.find(id).display_name
+                         end
+                       else
+                         object.creator
+                       end
   end
 end
