1928884357abdbc25873af9f7335d41f7f056157
Max Kadel <max@curationexperts.com>
2021-05-05 20:38:26 -0400

    Prep to test for nested creators through controller / form

--- app/forms/hyrax/cypripedium_form.rb
+++ app/forms/hyrax/cypripedium_form.rb
@@ -18,10 +18,27 @@ module Hyrax
 
     def self.model_attributes(form_params)
       result = super
-      result[:related_url].map! { |related_url| RDF::Markdown::Literal.new(related_url) }
-      result[:description].map! { |description| RDF::Markdown::Literal.new(description) }
-      result[:abstract].map! { |abstract| RDF::Markdown::Literal.new(abstract) }
+      result[:related_url]&.map! { |related_url| RDF::Markdown::Literal.new(related_url) }
+      result[:description]&.map! { |description| RDF::Markdown::Literal.new(description) }
+      result[:abstract]&.map! { |abstract| RDF::Markdown::Literal.new(abstract) }
       result
+    end
+
+    # This describes the parameters we are expecting to receive from the client
+    # @return [Array] a list of parameters used by sanitize_params
+    def self.build_permitted_params
+      super
+      # super + [
+      #   :on_behalf_of,
+      #   :version,
+      #   :add_works_to_collection,
+      #   {
+      #     creator_id_attributes: [:id, :_destroy],
+      #     based_near_attributes: [:id, :_destroy],
+      #     member_of_collections_attributes: [:id, :_destroy],
+      #     work_members_attributes: [:id, :_destroy]
+      #   }
+      # ]
     end
   end
 end
