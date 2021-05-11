d056987b56ac05297beb810f8a5819f59f91deba
Max Kadel <max@curationexperts.com>
2021-05-11 16:21:00 -0400

    Can remove creator_label when remove associated creator_id

--- app/assets/javascripts/creator_id.js
+++ app/assets/javascripts/creator_id.js
@@ -3,6 +3,7 @@ Blacklight.onLoad(function () {
       var elem = $(this)
       if (elem.data('autocomplete') === "creator_id") {
         addCreatorListener(elem.parent().parent());
+        addRemoveListener(elem.parent().parent());
       }
   }))
 })
@@ -10,7 +11,13 @@ Blacklight.onLoad(function () {
 function addCreatorListener(elem) {
   elem.on( "autocompleteselect", function( event, ui ) {
     let creator_label = ui.item.label
-    console.log("CYP with creator_label: " + creator_label)
     $("<input class=\"form-control\" value=\"" + creator_label + "\" readonly>").appendTo(event.currentTarget);
   } );
+}
+
+function addRemoveListener(elem) {
+  elem.on("click", ".remove", event => {
+    let creator_label = $(event.currentTarget).parent().parent().next();
+    creator_label.remove();
+  })
 }
