fa58fd5be34e4b79fa8db33aab97252a9b52e73b
Max Kadel <max@curationexperts.com>
2021-05-11 10:54:19 -0400

    Add listener to ul grandparent of creator_id

--- app/assets/javascripts/creator_id.js
+++ app/assets/javascripts/creator_id.js
@@ -4,12 +4,15 @@ Blacklight.onLoad(function () {
   $('[data-autocomplete]').each((function() {
       var elem = $(this)
       if (elem.data('autocomplete') === "creator_id") {
-        elem.on( "autocompleteselect", function( event, ui ) {
-          let creator_label = ui.item.label
-          console.log("hit CYP creator_label: " + creator_label)
-        } );
+        addCreatorListener(elem.parent().parent());
       }
   }))
-
-
 })
+
+// Right now this is only adding the listener to the first creator_id field
+function addCreatorListener(elem) {
+  elem.on( "autocompleteselect", function( event, ui ) {
+    let creator_label = ui.item.label
+    console.log("hit CYP creator_label: " + creator_label)
+  } );
+}
