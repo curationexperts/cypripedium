4fefe3e3c1294e19ee2f32f4159982c10195c2d4
Max Kadel <max@curationexperts.com>
2021-05-12 12:01:25 -0400

    Show creator_label on edit form
    - Display creator_label after each list item
    - No longer need listener for removing items, existing listener finds 
    relevant items
    
    Co-Authored-By: Rachel Kadel <rachel.kadel@curationexperts.com>

--- app/assets/javascripts/creator_id.js
+++ app/assets/javascripts/creator_id.js
@@ -1,27 +1,40 @@
 Blacklight.onLoad(function () {
   var listeners_added = false
+
   $('[data-autocomplete]').each((function() {
       var elem = $(this)
+      var creator_label = null;
 
       if (elem.data('autocomplete') === "creator_id" && listeners_added === false) {
-        console.log("hit CYP conditional with parent index: " + elem.parent().index())
-        addCreatorListener(elem.parent().parent());
-        addRemoveListener(elem.parent().parent());
+        addCreatorListener(elem.parent().parent(), elem);
         listeners_added = true
+      }
+
+      if (elem.data('autocomplete') === "creator_id") {
+        console.log("hit CYP conditional with parent index: " + elem.parent().index())
+        var creator_identifier = elem[0].value
+        console.log("creator_identifier_" + creator_identifier)
+
+        elem.addClass("creator_identifier_" + creator_identifier)
+        let test_url = "http://localhost:3000/authorities/show/creator_authority/" + creator_identifier
+        getCreatorLabel(test_url)
       }
   }))
 })
 
-function addCreatorListener(elem) {
-  elem.on( "autocompleteselect", function( event, ui ) {
+function addCreatorListener(grandparent, elem) {
+  grandparent.on( "autocompleteselect", function( event, ui ) {
     let creator_label = ui.item.label
-    $("<input class=\"form-control\" value=\"" + creator_label + "\" readonly>").appendTo(event.currentTarget);
+    $("<input class=\"form-control\" value=\"" + creator_label + "\" readonly>").insertAfter(event.target);
   } );
 }
 
-function addRemoveListener(elem) {
-  elem.on("click", ".remove", event => {
-    let creator_label = $(event.currentTarget).parent().parent().next();
-    creator_label.remove();
-  })
+function getCreatorLabel(url) {
+  $.get( url, successCallback);
+}
+
+function successCallback(data) {
+  creator_label = data.label;
+  console.log("Creator label: " + creator_label)
+  $(".creator_identifier_" + data["value"]).after($("<input class=\"form-control\" value=\"" + creator_label + "\" readonly>"));
 }
