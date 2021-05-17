4b0b638105c4caad2323b73a65147e80d7e2fc19
Rachel Kadel <rachel.kadel@curationexperts.com>
2021-05-17 13:44:24 -0400

    Ignore disabled creators (#481)
    * Exclude inactive creators from autocomplete suggestions
    
    * Filter out inactive creators in creator authority search
    
    * Set locale in creator model spec to address order-dependent failures
    
    * Extend authority request spec to include active-ness
    
    Co-authored-by: Rachel Kadel <rachel.kadel@curationexperts.com>
    Co-authored-by: Max Kadel <max@curationexperts.com>
    Co-authored-by: Mark Bussey <mark@curationexperts.com>

--- app/authorities/qa/authorities/creator_authority.rb
+++ app/authorities/qa/authorities/creator_authority.rb
@@ -4,7 +4,7 @@ module Qa::Authorities
     # Corresponds to endpoint authorities/search/creator_authority?q=[q]
     # e.g. authorities/search/creator_authority?q=Car
     def search(q)
-      results = Creator.where('lower(display_name) like ?', "#{q.downcase}%").limit(25)
+      results = Creator.where('active_creator AND lower(display_name) like ?', "#{q.downcase}%").limit(25)
       map_results(results)
     end
 
