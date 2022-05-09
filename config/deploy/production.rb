ce62aecdd747eda6fa8d1a614de844f33caf6cde
McClain Looney <mcclain@curationexperts.com>
2022-05-09 09:25:45 -0500

    Extracts ssm configuration
    The SSM deployment will not function in ansible localhost deployment
    situations, so a new SSM deploy environment was needed to preserve
    ansible capability.
    
    To deploy in an ssm environment, "cap ssm deploy" after obtaining ssm
    credentials locally

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 # deploys to FRBM AWS production
-require 'net/ssh/proxy/command'
 set :stage, :production
-set :instance_id, lambda {
-  `aws ec2 describe-instances --region us-gov-east-1  --profile frbm-ssm\
-                       --filters Name=tag-key,Values=Environment Name=tag-value,Values=prod Name=instance-state-name,Values=running \
-                       --query "Reservations[*].Instances[*].InstanceId" --output text`.chomp
-}
-server fetch(:instance_id), user: 'deploy', roles: [:web, :app, :db]
-set :command, %(aws --profile frbm-ssm  ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p')
-set :ssh_options,
-  auth_methods: %w[publickey],
-  forward_agent: true,
-  proxy: Net::SSH::Proxy::Command.new(fetch(:command))
-
 set :rails_env, 'production'
+server 'localhost', user: 'deploy', roles: [:web, :app, :db]
