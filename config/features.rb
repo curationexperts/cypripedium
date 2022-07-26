80644b26ce567a20f20922fd4e6b7f7389082ac2
Norm Orstad <norman@orstad.com>
2022-07-26 11:52:53 -0500

    Feature flipper for analytics

---
+++ config/features.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+Flipflop.configure do
+  feature :sidebar_analytics,
+          default: false,
+          description: "Show analytics in the sidebar. Configuration needed"
+end
