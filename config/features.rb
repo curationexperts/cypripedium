2ec622597d7ac38efcf20c263f6e6186097d8a1e
Norm Orstad <norman@orstad.com>
2022-07-26 17:02:08 -0500

    Uses hyrax config rather than feature flipper

--- config/features.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-Flipflop.configure do
-  feature :sidebar_analytics,
-          default: false,
-          description: "Show analytics in the sidebar. Configuration needed"
-end
