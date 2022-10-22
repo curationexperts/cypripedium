6816627542029e3c284a501259b67aa66fcbd8eb
MPLSFedResearchTZ <tao.zhao@mpls.frb.org>
2022-10-22 01:52:51 -0500

    fixed the issue of undefined method html_attributes

--- app/helpers/collection_license_attribute_renderer_helper.rb
+++ app/helpers/collection_license_attribute_renderer_helper.rb
@@ -13,12 +13,21 @@ module CollectionLicenseAttributeRendererHelper
                    else
                      %(<a href=#{ERB::Util.h(value)} target="_blank">#{Hyrax.config.license_service_class.new.label(value)}</a>)
                    end
-    license_html = "<span#{html_attributes(microdata_value_attributes(:license))}>#{license_html}</span>" if microdata_value_attributes(:license).present?
+    license_html = "<span#{license_attributes(microdata_value_attributes(:license))}>#{license_html}</span>" if microdata_value_attributes(:license).present?
     text += "<dd>#{license_html}</dd>"
     # rubocop:disable Rails/OutputSafety
     text.html_safe
     # rubocop:enable Rails/OutputSafety
   rescue => e
     Rails.logger.error "An error came up while rendering collection license field: " + e.message
+  end
+
+  private
+
+  def license_attributes(attributes)
+    attributes.map do |key, value|
+      value_set = value.present? ? %(="#{value}") : nil
+      " #{key}#{value_set}"
+    end.join
   end
 end
