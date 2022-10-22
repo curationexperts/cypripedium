80b7db9e8222d55fd31a0bb01d0f7771ca38808c
MPLSFedResearchTZ <tao.zhao@mpls.frb.org>
2022-10-22 01:22:29 -0500

    Changed license text into link for dashboard collection info page

---
+++ app/helpers/collection_license_attribute_renderer_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module CollectionLicenseAttributeRendererHelper
+  include Hyrax::Renderers::ConfiguredMicrodata
+  def render_collection_license_attribute(presenter)
+    label = collection_metadata_label(presenter, :license)
+    text = ""
+    text += "<dt>#{label}</dt>"
+    value = presenter.license.at(0)
+    parsed_uri = URI.parse(value)
+    license_html = if parsed_uri.nil?
+                     ERB::Util.h(value)
+                   else
+                     %(<a href=#{ERB::Util.h(value)} target="_blank">#{Hyrax.config.license_service_class.new.label(value)}</a>)
+                   end
+    license_html = "<span#{html_attributes(microdata_value_attributes(:license))}>#{license_html}</span>" if microdata_value_attributes(:license).present?
+    text += "<dd>#{license_html}</dd>"
+    # rubocop:disable Rails/OutputSafety
+    text.html_safe
+    # rubocop:enable Rails/OutputSafety
+  rescue => e
+    Rails.logger.error "An error came up while rendering collection license field: " + e.message
+  end
+end
