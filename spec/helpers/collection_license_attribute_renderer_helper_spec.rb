3aa061875cd79056b70d37609a2457ac2bbb1f07
MPLSFedResearchTZ <tao.zhao@mpls.frb.org>
2022-11-09 09:00:11 -0600

    Removed the if statement and added another test case

--- spec/helpers/collection_license_attribute_renderer_helper_spec.rb
+++ spec/helpers/collection_license_attribute_renderer_helper_spec.rb
@@ -6,7 +6,7 @@ include CollectionLicenseAttributeRendererHelper
 RSpec.describe 'Edit markdown fields:', type: :system, js: true do
   COLLECTION_LICENSE_ATTRIBUTE_HTML = '<dt>License</dt><dd><a href=https://creativecommons.org/licenses/by-nc/4.0/ ' \
   'target="_blank">Creative Commons BY-NC Attribution-NonCommercial 4.0 International</a></dd>'
-  COLLECTION_LICENSE_ATTRIBUTE_HTML_MICRODATA = '<dt>License</dt><dd><span itemprop="name"><a href=https://creativecommons.org/licenses/' \
+  COLLECTION_LICENSE_ATTRIBUTE_HTML_DISPLAY_MICRODATA = '<dt>License</dt><dd><span itemprop="name"><a href=https://creativecommons.org/licenses/' \
   'by-nc/4.0/ target="_blank">Creative Commons BY-NC Attribution-NonCommercial 4.0 International</a></span></dd>'
 
   context 'displaying a custom collection' do
@@ -23,13 +23,15 @@ RSpec.describe 'Edit markdown fields:', type: :system, js: true do
     let(:solr_document) { SolrDocument.new(collection) }
     let(:presenter) { Hyrax::CollectionPresenter.new(solr_document, ability) }
 
-    it 'veryfy the html output of license field' do
+    it 'veryfy the html output of a collection with display_microdata == false' do
+      Hyrax.config.display_microdata = false
       text = render_collection_license_attribute(presenter)
-      if microdata_value_attributes(:license).present?
-        expect(text).to eq COLLECTION_LICENSE_ATTRIBUTE_HTML_MICRODATA
-      else
-        expect(text).to eq COLLECTION_LICENSE_ATTRIBUTE_HTML
-      end
+      expect(text).to eq COLLECTION_LICENSE_ATTRIBUTE_HTML
+    end
+
+    it 'veryfy the html output of a collection with a display_microdata == true' do
+      text = render_collection_license_attribute(presenter)
+      expect(text).to eq COLLECTION_LICENSE_ATTRIBUTE_HTML_DISPLAY_MICRODATA
     end
   end
 end
