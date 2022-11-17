bc08a259d076a0d4e6959fcbe1c10469122d47cb
MPLSFedResearchTZ <tao.zhao@mpls.frb.org>
2022-11-17 11:18:26 -0600

    Removed printing code

--- config/initializers/collection_search_builder_modifier.rb
+++ config/initializers/collection_search_builder_modifier.rb
@@ -3,7 +3,6 @@
 Hyrax::CollectionSearchBuilder.class_eval do
   def add_sorting_to_solr(solr_parameters)
     return if solr_parameters[:q]
-    sort = solr_parameters[:sort] ||= blacklight_params[:sort].presence || "#{sort_field} asc"
-    puts '******* ' + sort
+    solr_parameters[:sort] ||= blacklight_params[:sort].presence || "#{sort_field} asc"
   end
 end
