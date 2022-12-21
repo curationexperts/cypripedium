da22f2120bcffd21f528012b20b3ea72c4ca0d1c
MPLSFedResearchTZ <tao.zhao@mpls.frb.org>
2022-12-21 09:55:46 -0600

    Fixed the locale issue in spec files and changed the 'Conference' to be 'conference'

--- spec/system/404_spec.rb
+++ spec/system/404_spec.rb
@@ -7,17 +7,21 @@ RSpec.describe 'Getting a 404 for RecordNotFound', type: :system do
   end
   context 'visiting a work that does not exist' do
     it 'has a 404 page' do
+      default_locale = I18n.locale
       visit('/concern/works/s7526c41m?locale=pt-BR')
       expect(page).to have_content('does not exist')
       expect(page).to have_content('You may have mistyped the address or the page may have moved.')
+      I18n.locale = default_locale
     end
   end
 
   context 'visiting a route with no content' do
     it 'has a 404 page' do
+      default_locale = I18n.locale
       visit('/bla/bla/bla')
       expect(page).to have_content('does not exist')
       expect(page).to have_content('You may have mistyped the address or the page may have moved.')
+      I18n.locale = default_locale
     end
   end
 end
--- spec/system/invalid_locale_spec.rb
+++ spec/system/invalid_locale_spec.rb
@@ -8,17 +8,21 @@ RSpec.describe 'Getting a friendly error for Invalid Locales', type: :system do
   end
   context 'visiting a locale that does not exist' do
     it 'has a 406 page' do
+      default_locale = I18n.locale
       visit('/?locale=pluto')
       expect(page).to have_content('Research Database')
       expect(page).to have_content('Error: Invalid language selection')
       expect(page).to have_content('Please select a valid language from the selector')
+      I18n.locale = default_locale
     end
   end
 
   context 'visiting a valid locale' do
     it 'gives the content in the correct language' do
+      default_locale = I18n.locale
       visit('/?locale=en')
       expect(page).to have_content('Welcome to the Research Database')
+      I18n.locale = default_locale
     end
   end
 end
