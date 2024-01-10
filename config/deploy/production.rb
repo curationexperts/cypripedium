a5e62889e23eb15865f054012f9853aa5ba31b6d
Mark Bussey <mark@curationexperts.com>
2024-01-10 11:35:42 -0800

    Simplify Capistrano / Sidekiq integration (#571)
    This change updates the `capistrano-sidekiq` gem and removes
    obsolete configuration that is now included in the gem.
    
    We've also removed Capistrano stages for legacy vanilla EC2 instances
    because all deployments must now occur via SSM to Gov Cloud.

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# deploys to FRBM AWS production
-set :stage, :production
-set :rails_env, 'production'
-server '3.208.81.15', user: 'deploy', roles: [:web, :app, :db]
--- config/deploy/ssm.rb
+++ config/deploy/ssm.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-# deploys to FRBM AWS production
+# deploys to FRBM AWS Gov Cloud instances vis SSM
+# defaults to staging environment
+# To set a target environment, the instance must be tagged in Gov Cloud:
+#   Project=rdb
+#   Environment=prod | stage | qa | etc.
+# Invoke the deploy command with the HOST_ENV variable specifying the desired environment, e.g.
+#   HOST_ENV=prod bundle exec cap ssm deploy
+
 require 'net/ssh/proxy/command'
 set :stage, :production
-set :host_env, -> { ENV['host_env'] || 'prod' }
+set :host_env, -> { ENV['HOST_ENV'] || 'stage' }
 set :instance_id, lambda {
   `aws ec2 describe-instances --region us-gov-east-1  --profile frbm-ssm \
                        --filters Name=tag-key,Values=Environment Name=tag-value,Values=#{fetch(:host_env)} Name=instance-state-name,Values=running \
