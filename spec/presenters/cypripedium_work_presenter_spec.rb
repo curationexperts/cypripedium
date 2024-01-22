84ccc3ea7c57c4d9e931ae9e14da478c7fd92c0f
Mark Bussey <mark@curationexperts.com>
2024-01-22 15:46:33 -0800

    Update citations to use CSL sourced citation formats (#578)
    This change refactors citations to use style definitions based
    on the open source [Citation Style Language (CSL) project](https://citationstyles.org/).
    This allows us to avoid having to override hand-crafted legacy style
    definitions defined in Hyrax which have generally not been updated for
    8 years.  It also moves the functionality from view helpers into
    the work presenter to help better isolate this feature in the codebase.
    
    Most importantly, using shared CSL style definitions allows us to
    take advantage of a well maintaned repository of open source style definitions
    from the [Citation Style Language - Style Repository](https://github.com/citation-style-language/styles#citation-style-language---style-repository)
    
    ACKNOWLEDGEMENTS
    This code was heavily inspired by work by Brad Watson at Emory University
    Libraries https://github.com/emory-libraries/dlp-lux/pull/552 which was in
    turn inspired by Tamsin Johnson https://github.com/curationexperts/mahonia/pull/156
    
    REFERENCES
    * https://citationstyles.org/
    * https://docs.citationstyles.org/en/stable/specification.html
    * https://github.com/citation-style-language/styles/blob/master/chicago-fullnote-bibliography.csl
    * https://github.com/inukshuk/citeproc-ruby

--- spec/presenters/cypripedium_work_presenter_spec.rb
+++ spec/presenters/cypripedium_work_presenter_spec.rb
@@ -3,11 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe CypripediumWorkPresenter do
-  let(:presenter) { described_class.new(solr_document, ability, request) }
-
-  let(:solr_document) { SolrDocument.new(id: '123') }
-  let(:ability) { nil }
-  let(:request) { nil }
+  let(:presenter) { described_class.new(solr_document, nil, request) }
+  let(:request) { instance_double(ActionDispatch::Request, base_url: 'example.org') }
+  let(:solr_document) { SolrDocument.new(id: '123', has_model_ssim: ['Publication']) }
 
   it_behaves_like 'a work presenter'
+
+  describe 'citation' do
+    it 'renders in chicago format' do
+      expect(presenter.chicago_citation).not_to be_blank
+    end
+
+    it 'renders in apa format' do
+      expect(presenter.apa_citation).not_to be_blank
+    end
+
+    it 'renders in mla format' do
+      expect(presenter.mla_citation).not_to be_blank
+    end
+  end
 end
