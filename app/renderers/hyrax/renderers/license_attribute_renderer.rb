fc25c5a09b90a1611227f205b188e92c7c11247a
Mark Bussey <mark@curationexperts.com>
2024-08-14 16:33:17 -0500

    Address potential "tabnabbing" vulnerability
    **ISSUE**
    FRB Security Scans have identified links to external sites opened in new
    windows (i.e. `target='_blank'`) as a potential security threat.
    
    Examples of these links appear in the Research Database footer such as:
    ```
    <a class="navbar-link" target="_blank" href="https://minneapolisfed.org">Minneapolis Fed Home</a>
    <a class="navbar-link" target="_blank" href="https://minneapolisfed.org/site-information/disclaimer">Disclaimer</a>
    <a class="navbar-link" target="_blank" href="https://minneapolisfed.org/site-information/privacy-policies">Privacy Policy</a>
    <a href="https://www.minneapolisfed.org/economic-research" class="navbar-link" target="_blank">Federal Reserve Bank of Minneapolis Research Division</a>
    ```
    
    For further details, see https://cwe.mitre.org/data/definitions/1022.html
    
    **REOLUTION**
    Although all of these links are to other trusted FRBM sites, modern
    accessibilty practices discourage opening content in new pages since this
    can create a disorenting experience for users not expecting to move to a new
    window (e.g. the browser back function no longer works as expected).
    
    We've chosen to eliminate the new window targets and simply open the page
    in the same browser window because:
    1. All modern browsers provide an easy mechanism that allows users to choose
    wether to open the link it the same window or a new window or tab, typically
    by using a modifier key when clicking the link.
    2. The links in question are relatively low use footer links, and pose minimal
    disruption to workflow if opened in the current window.
    
    **OTHER REFERENCES**
    * https://theadminbar.com/accessibility-weekly/opening-links-in-new-tabs-or-not/
    * https://www.w3.org/TR/WCAG21/#change-on-request

--- app/renderers/hyrax/renderers/license_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/license_attribute_renderer.rb
@@ -19,7 +19,7 @@ module Hyrax
         license_html = if parsed_uri.nil?
                          ERB::Util.h(value)
                        else
-                         %(<a href=#{ERB::Util.h(value)} target="_blank">#{Hyrax.config.license_service_class.new.label(value)}</a>)
+                         %(<a href=#{ERB::Util.h(value)}>#{Hyrax.config.license_service_class.new.label(value)}</a>)
                        end
         if microdata_value_attributes(field).present?
           "<span#{html_attributes(microdata_value_attributes(field))}>#{license_html}</span>"
