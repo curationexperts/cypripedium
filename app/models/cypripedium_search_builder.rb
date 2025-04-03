d2cd191aaa90708d3ae56ebe40e00d09ce3ffff4
Mark Bussey <mark@curationexperts.com>
2025-04-02 19:23:50 -0500

    Remove duplicate and expensive processors
    **ISSUE**
    A number of catalog search filters add excess processing time but
    don't change the results in our application context - e.g. join
    searches for files.
    
    **RESOLUTION**
    Remove high cost (i.e. slow) filters from the default catalog processor
    chain.

--- app/models/cypripedium_search_builder.rb
+++ app/models/cypripedium_search_builder.rb
@@ -6,4 +6,8 @@ class CypripediumSearchBuilder < Hyrax::CatalogSearchBuilder
   # Cypripedium does not display a collection facet, so we want to remove this
   # filter from the processor chain used in the CatalogController
   self.default_processor_chain.delete(:filter_collection_facet_for_access)
+  self.default_processor_chain.delete(:show_works_or_works_that_contain_files)
+  self.default_processor_chain.delete(:add_query_to_solr)
+  self.default_processor_chain.delete(:show_only_active_records)
+  self.default_processor_chain.uniq!
 end
