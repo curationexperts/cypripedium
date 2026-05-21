07c8977cb85eeadd89d6150f877e7eec0b51bd7c
Mark Bussey <mark@curationexperts.com>
2026-05-21 14:51:27 -0500

    Patch Qualys flagged unencoded parameters
    **ISSUE**
    Qualys scans report a large number of potential unencoded character
    vulnerabilites. We believe that the application sufficiently protects
    from the potential threat vector, but we want to eliminate the
    issues from Qualys report to achieve a clean baseline.
    
    **RESOLUTION**
    1) Ensure query parameters are escaped before reflecting them back in
    search output.
    2) Clear any invalid query parameters on invalid requests.
    
    **Qualys Details**
    >**Threat**
    >The web application reflects potentially dangerous characters such as single quotes, double quotes, and angle brackets. These characters are commonly used for HTML injection attacks such as cross-site scripting (XSS).
    >**Impact**
    >No exploit was determined for these reflected characters. The input parameter should be manually analyzed to verify that no other characters can be injected that would lead to an HTML injection (XSS) vulnerability.
    >**Solution**
    >Review the reflected characters to ensure that they are properly handled as defined by the web application's coding practice. Typical solutions are to apply HTML encoding or percent encoding to the characters depending on where they are placed in the HTML. For example, a double quote might be encoded as " when displayed in a text node, but as %22 when placed in the value of an href attribute.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@
 
 class PagesController < ApplicationController
   def error_404 # rubocop:disable Naming/VariableNumber
+    # Clear params so we don't reflect any injection attacks
+    params.slice!(:controller, :action, :path)
     flash[:alert] = 'The page you are looking for does not exist.'
     render file: Rails.public_path.join('404.html'), status: :not_found
   end
